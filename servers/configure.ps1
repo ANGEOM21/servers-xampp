d150b49e367c11933f299ee7de8ff89a72956652
Angeom21 <93632225+ANGEOM21@users.noreply.github.com>
2023-08-27 12:25:14 +0700

    Update configure.ps1

--- servers/configure.ps1
+++ servers/configure.ps1
@@ -10,32 +10,41 @@ if (Test-Path "C:\xampp") {
 
 $currentPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
 
-Write-Host "Sedang Menjalankan File Configurasi."
+Write-Host -ForegroundColor Cyan "Sedang Menjalankan File Configurasi."
 Start-Sleep -Seconds 2
 if ($currentPath -notlike "*$newPaths*") {
     $newPath = "$currentPath;$newPaths"
     [Environment]::SetEnvironmentVariable("PATH", $newPath, "Machine")
-    Write-Host "Path telah berhasil ditambahkan."
+    Write-Host -ForegroundColor Green "Path telah berhasil ditambahkan."
 
-    Write-Host ""
-    Write-Host ""
-    $startServersPath = Join-Path -Path $PSScriptRoot -ChildPath "servers\bin\start_servers.bat"
-    $stopServersPath = Join-Path -Path $PSScriptRoot -ChildPath "servers\bin\stop_servers.bat"
-    
-    $destinationDirectory = $newPaths
-    
-    Move-Item -Path $startServersPath -Destination $destinationDirectory
-    Move-Item -Path $stopServersPath -Destination $destinationDirectory
-    
-    Write-Host "Configurasi Selesai"
-    Write-Host ""
-    Write-Host ""
 } else {
-    Write-Host "Path sudah ada dalam variabel lingkungan."
+    Write-Host -ForegroundColor Red "Path sudah ada dalam variabel lingkungan !!."
+}
+Write-Host ""
+$startServersPath = Join-Path -Path $PSScriptRoot -ChildPath "bin\start_servers.bat"
+$stopServersPath = Join-Path -Path $PSScriptRoot -ChildPath "bin\stop_servers.bat"
+
+$destinationDirectory = $newPaths
+Write-Host -ForegroundColor Yellow "Sedang Memindahkan file"
+Start-Sleep -Seconds 2
+
+if (Test-Path $startServersPath) {
+    Move-Item -Path $startServersPath -Destination $destinationDirectory
+    Write-Host "File start_servers.bat telah dipindahkan ke $destinationDirectory"
+} else {
+    Write-Host -ForegroundColor Red "File start_servers.bat tidak ditemukan di direktori bin."
 }
 
+if (Test-Path $stopServersPath) {
+    Move-Item -Path $stopServersPath -Destination $destinationDirectory
+    Write-Host "File stop_servers.bat telah dipindahkan ke $destinationDirectory"
+} else {
+    Write-Host -ForegroundColor Red "File stop_servers.bat tidak ditemukan di direktori bin."
+}
+
+Start-Sleep -Seconds 2
+Write-Host -ForegroundColor Green "Configurasi Selesai"
 
 
-# author : Angeom21
 
-
+#author: An_geom
